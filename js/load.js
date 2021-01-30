26c4fcc1dd7b5649003ed24b01a300a7a61d24e4
gchrgeorgiou <george.chrgeorgiou@bankofcyprus.com>
2021-01-30 17:45:23 +0200

    generic form

--- js/load.js
+++ js/load.js
@@ -3,26 +3,18 @@
 document.getElementById('cmbDiagram').onchange = function () {
     var selVal = document.getElementById('cmbDiagram').value;
     if (selVal == "") {
-        document.getElementById("loadMore").disabled = true;
+        document.getElementById("graphplaceholder").innerHTML = "";
+        alert('Please select a diagram!');
     }
     else {
-        document.getElementById("loadMore").disabled = false;
+        document.getElementById("graphplaceholder").innerHTML = "";
+        var selection = document.getElementById('cmbDiagram');
+        var jsonpath = "data/" + selection.value;
+        var chartTitle = selection.options[selection.selectedIndex].text;
+        anychart.data.loadJsonFile(jsonpath, function (data) {
+            //console.log(data);
+            drawChart(chartTitle, data, "Fixed");
+        });
     }
 
-};
-
-
-document.getElementById("loadMore").onclick = function () {
-    document.getElementById("graphplaceholder").innerHTML = "";
-    var selection = document.getElementById('cmbDiagram');
-    var jsonpath = "data/" + selection.value;
-    var chartTitle = selection.options[selection.selectedIndex].text;
-
-    
-
-
-    anychart.data.loadJsonFile(jsonpath, function (data) {
-        //console.log(data);
-        drawChart(chartTitle, data,"Fixed");
-    });
 };
